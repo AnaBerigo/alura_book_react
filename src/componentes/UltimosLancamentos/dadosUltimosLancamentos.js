e38dc3503759b624953dd09236dedd4bcd3203eb
Ana Berigo <ana.berigo17@gmail.com>
2025-01-23 13:51:26 -0300

    feat: section "ultimos lançamentos" criada

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,11 @@
+import livro from "../../imagens/livro.png";
+
+export const livros = [
+  { nome: "Liderança em Design", src: livro, id: 1 },
+  { nome: "JavaScript Assertivo", src: livro, id: 2 },
+  {
+    nome: "Cangaceiro JavaScript: Uma aventura no sertão da programação",
+    src: livro,
+    id: 3,
+  },
+];
