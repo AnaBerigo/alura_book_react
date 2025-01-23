6bf1ccd622284b9b3b85c308cc43b0939b7eed88
Ana Berigo <ana.berigo17@gmail.com>
2025-01-23 09:50:47 -0300

    feat: filtrando livros por nome (exibindo apenas no console)

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,34 @@
+import livro from "../../imagens/livro.png";
+
+export const livros = [
+  {
+    nome: "Guia Front-End",
+    id: 1,
+    src: livro,
+  },
+  {
+    nome: "A Web Mobile",
+    id: 2,
+    src: livro,
+  },
+  {
+    nome: "HTML5 e CSS3",
+    id: 3,
+    src: livro,
+  },
+  {
+    nome: "Coleção Cangaceiro JavaScript",
+    id: 4,
+    src: livro,
+  },
+  {
+    nome: "Segurança em front-end",
+    id: 5,
+    src: livro,
+  },
+  {
+    nome: "Coletânea Front-end",
+    id: 6,
+    src: livro,
+  },
+];
